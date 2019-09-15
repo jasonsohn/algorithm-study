c6b10125109f85ea934f5acb9d0416474056e4c3
Jason <jason.sohn28@gmail.com>
2019-09-15 22:02:58 +0900

    3단계 for문 구구단 문제 커밋

--- src/main/java/baekjoon/problemByStep/step03/Step03_01_2739_Multiplication.java
+++ src/main/java/baekjoon/problemByStep/step03/Step03_01_2739_Multiplication.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  * 입력 : 첫째 줄에 N이 주어진다. N은 1보다 크거나 같고, 9보다 작거나 같다.
  * 출력 : 출력형식과 같게 N*1부터 N*9까지 출력한다.
  */
-public class Step03_01_2739_multiplication {
+public class Step03_01_2739_Multiplication {
 
     public static void main(String[] args) {
         inputData();
